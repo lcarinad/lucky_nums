bad392a86fc99da03f041333d0d2ae668bd2d562
Lauren DeLago <laurendelago@Laurens-Mini-22.home>
2023-12-05 21:13:02 -0500

    post method

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, jsonify
-
+from flask import Flask, render_template, request, jsonify, flash
+from sqlalchemy.exc import IntegrityError
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -9,6 +9,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "oh-so-secret"
 
 connect_db(app)
+
+app.app_context().push()
 
 @app.route("/")
 def homepage():
@@ -24,34 +26,12 @@ def homepage():
 def create_lucky_num_profile():
     """Create lucky num from user form data & return it.
     Return JSON {'user':{id, name, email, year, color}}"""
-    data = request.get_json()
 
-    #validation for required fields
-    errors = {}
-    if 'name' not in data:
-        errors['name'] = ['This field is required']
-    if 'email' not in data:
-        errors['email'] = ['This field is required']
-    if 'year' not in data:
-        errors['year'] = ['This field is required']
-    if 'color' not in data:
-        errors['color'] = ['This field is required']
-    elif data['color'] not in ['red', 'green', 'orange', 'blue']:
-        errors['color'] = ['Invalid value, must be red, green, orange, or blue']
-    
-    if errors:
-        return jsonify({"errors": errors}), 400
-
-    #if validations pass, create and save User
-    name = data["name"]
-    email = data["email"]
-    year = data["year"]
-    color = data["color"]
-
-    new_user = User(name=name, email=email, year=year, color=color)
+    new_user=User.validate()
 
     db.session.add(new_user)
     db.session.commit()
     response_json = jsonify(new_user.serialize())
 
     return (response_json, 201)
+    
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy, CheckConstraint
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+from flask import jsonify, request
 
 db = SQLAlchemy()
 
@@ -15,10 +17,37 @@ class User(db.Model):
     email = db.Column(db.Text, nullable=False, unique=True)
     color = db.Column(db.Text, nullable = False)
 
-    __table_args__ = (
-        CheckConstraint('year BETWEEN 1900 AND 2000', name='check_birth_year'),CheckConstraint("color IN ('red', 'green', 'orange', 'blue')", name="check_color"),
+    __table_args__ = ( CheckConstraint('year BETWEEN 1900 AND 2000', name='check_birth_year'), CheckConstraint("color IN ('red', 'green', 'orange', 'blue')", name="check_color")
     )
-    
+
+    @classmethod
+    def validate(cls):
+        """Validates user submitted data"""
+        errors = {}
+        if 'name' not in request.json:
+            errors['name'] = ['This field is required']
+        if 'email' not in request.json:
+            errors['email'] = ['This field is required']
+        if 'year' not in request.json:
+            errors['year'] = ['This field is required']
+        elif request.json['year'] not in range(1900,2000):
+            errors['year'] = ['Invalid year. Birth year should be between 1900-2000']
+        if 'color' not in request.json:
+            errors['color'] = ['This field is required']
+        elif request.json['color'] not in ['red', 'green', 'orange', 'blue']:
+            errors['color'] = ['Invalid value, must be red, green, orange, or blue']
+        
+        if errors:
+            return jsonify({"errors": errors}), 400
+
+        #if validations pass, create and save User
+        name = request.json["name"]
+        email = request.json["email"]
+        year = request.json["year"]
+        color = request.json["color"]
+
+        new_user = User(name=name, email=email, year=year, color=color)
+        return new_user
 
     def serialize(self):
         """Returns a dict representation of user instance, which allows us to turn to JSON.  From the dict representation, the instance can be jsonified"""
